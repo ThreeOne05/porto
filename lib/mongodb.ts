76ca27c296d08df042548549445aa579436414d5
ThreeOne05 <asrulaidil45@gmail.com>
2025-06-10 23:47:08 +0800

    fix: update MongoDB connection settings for Vercel/Atlas

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -14,14 +14,17 @@ declare global {
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
+// Explicitly set TLS = true if running in production (Vercel) or always
+const options = { tls: true };
+
 if (process.env.NODE_ENV === "development") {
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri);
+    client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
   }
-  clientPromise = global._mongoClientPromise;
+  clientPromise = global._mongoClientPromise!;
 } else {
-  client = new MongoClient(uri);
+  client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
